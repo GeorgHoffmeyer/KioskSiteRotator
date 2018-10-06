78d601e6ebc608c461f8a7b4c465d9b8610c8ce8
Georg Hoffmeyer <georg.hoffmeyer@outlook.com>
2018-10-06 13:46:30 +0200

    load settings from url

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 
-var urls = [{
+var urls;
+/* = [{
     url: "https://www.google.de",
     duration: 5000
 },
@@ -11,22 +12,45 @@ var urls = [{
     url: "https://www.yahoo.com",
     duration: 5000
 }];
+*/
 
 var currentIndex = 0;
 var activeTabId = null;
 const alarmName = "switchSite";
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    log("Button clicked");
-    log("current Tab Id " + tab.id);
+    debug("Button clicked");
+    debug("current Tab Id " + tab.id);
 
     if (activeTabId == null) {
-        log("activated for Tab " + activeTabId);
+        debug("activated for Tab " + activeTabId);
         activeTabId = tab.id;
         init();
     } else {
-        log("deactivated");
+        debug("deactivated");
         activeTabId = null;
+    }
+});
+
+
+chrome.runtime.onInstalled.addListener(() => {
+    init();
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+
+    if (alarm.name == alarmName && activeTabId) {
+        var urlObj = urls[currentIndex];
+
+        debug("URL: " + urlObj.url + " duration: " + urlObj.duration);
+
+        chrome.tabs.executeScript(activeTabId, {
+            code: 'window.location.href = "' + urlObj.url + '"'
+        });
+
+        chrome.alarms.create(alarmName, { when: Date.now() + 5000 });
+
+        increseCurrentIndex();
     }
 });
 
@@ -42,8 +66,12 @@ function formatDate(date) {
     return year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
+function debug(message) {
+    console.debug(formatDate(new Date()) + " [background.js - DEBUG] " + message);
+}
+
 function log(message) {
-    console.log(formatDate(new Date()) + " [background.js] " + message);
+    console.log(formatDate(new Date()) + " [background.js - LOG] " + message);
 }
 
 function increseCurrentIndex() {
@@ -51,31 +79,23 @@ function increseCurrentIndex() {
     if (currentIndex >= urls.length) {
         currentIndex = 0;
     }
-    log("CurrentIndex: " + currentIndex);
+    debug("CurrentIndex: " + currentIndex);
 }
 
 function init() {
-    log("init called");
+    debug("init called");
+    loadSettings();
     chrome.alarms.create(alarmName, { when: Date.now() });
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-    init();
-});
-
-chrome.alarms.onAlarm.addListener(function (alarm) {
-
-    if (alarm.name == alarmName && activeTabId) {
-        var urlObj = urls[currentIndex];
-
-        log("URL: " + urlObj.url + " duration: " + urlObj.duration);
-
-        chrome.tabs.executeScript(activeTabId, {
-            code: 'window.location.href = "' + urlObj.url + '"'
-        });
-
-        chrome.alarms.create(alarmName, { when: Date.now() + 5000 });
-
-        increseCurrentIndex();
+function loadSettings() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://georghoffmeyer.files.wordpress.com/2018/10/url_sample.pdf", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            // JSON.parse does not evaluate the attacker's scripts.
+            urls = JSON.parse(xhr.responseText);
+        }
     }
-});
+    xhr.send();
+}
