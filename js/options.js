93d77f02343869bf69078d65cc307d8aebd99b4a
Georg Hoffmeyer <georg.hoffmeyer@1und1.de>
2018-10-08 08:44:10 +0200

    added localdata

--- js/options.js
+++ js/options.js
@@ -2,10 +2,12 @@
 function save_options() {
   var settingsurl = document.getElementById('settingsurl').value;
   var loadfromserver = $('#loadfromserver')[0].checked;
-  alert(settingsurl)
+  var localdata = $('#localdata').val();
+  
   chrome.storage.sync.set({
     settingsurl: settingsurl,
-    loadfromserver: loadfromserver
+    loadfromserver: loadfromserver,
+    localdata: localdata
   }, function () {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -22,9 +24,11 @@ function restore_options() {
   // Use default settingsurl as empty string
   chrome.storage.sync.get({
     settingsurl: "",
-    loadfromserver: false
+    loadfromserver: false,
+    localdata: '{"fullscreen": true,"urls": [{"url": "https://www.google.de","duration": 5000},{"url": "https://www.bing.com","duration": 5000},{"url": "https://www.yahoo.com","duration": 5000}]}'
   }, function (items) {
     document.getElementById('settingsurl').value = items.settingsurl;
+	$('#localdata').val(items.localdata);
     if(loadfromserver) {
       $('#loadfromserver')[0].checked = true;
     }
